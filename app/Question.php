7b4c0bedf01c980ef77dd622a86e828caa077a8a
sunitha <sg397@njit.edu>
2018-12-13 12:46:15 -0500

    Userstory2: Added view_count to questions table and QuestionsTableSeeder with default value 0

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $fillable = ['body'];
+    protected $fillable = ['body', 'view_count'];
 
     public function user()
     {
